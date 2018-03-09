a606dca723a11b9e9fde19d7ef45e2772f864922
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-03-09 14:59:52 -0500

    added items in list

--- src/mobile/DotnetNewMobile/TemplatePacksPage.xaml.cs
+++ src/mobile/DotnetNewMobile/TemplatePacksPage.xaml.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using DotnetNewMobile.ViewModels;
 using Xamarin.Forms;
 
 namespace DotnetNewMobile
 {
     public partial class TemplatePacksPage : ContentPage
     {
+        TemplatePacksViewModel viewModel;
         public TemplatePacksPage()
         {
             InitializeComponent();
+
+            BindingContext = viewModel = new TemplatePacksViewModel();
         }
 
-    }
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+            if(viewModel.Items.Count <= 0){
+                viewModel.LoadItemsCommand.Execute(null);
+            }
+		}
+	}
 }
