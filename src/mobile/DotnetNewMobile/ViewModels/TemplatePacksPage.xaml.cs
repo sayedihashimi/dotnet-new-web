0b3e337b8198c43ea9ed3aeb72a0f3dc187167fe
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-04-06 11:01:13 -0400

    fix to refresh templates after update

--- src/mobile/DotnetNewMobile/ViewModels/TemplatePacksPage.xaml.cs
+++ src/mobile/DotnetNewMobile/ViewModels/TemplatePacksPage.xaml.cs
@@ -21,9 +21,7 @@ namespace DotnetNewMobile
 		protected override void OnAppearing()
 		{
 			base.OnAppearing();
-            if(viewModel.Items.Count <= 0){
-                viewModel.LoadItemsCommand.Execute(null);
-            }
+            viewModel.LoadItemsCommand.Execute(null);
 		}
 	}
 }
