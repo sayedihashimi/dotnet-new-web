62bf52baea14574e60ca8affea01e72a1797cabc
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-07-18 23:23:17 -0400

    wip

---
+++ src/TemplatesConsole/HelloCommand.cs
@@ -0,0 +1,33 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using SayedHa.Commands.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplatesConsole {
+    public class HelloCommand<T> : BaseCommandLineApplication<T> where T:class{
+        public HelloCommand() : base(
+            "hello",
+            "helloCommand",
+            "Says hello") {
+
+            this.OnExecute(() => {
+                Console.WriteLine("hello");
+                return 1;
+            });
+
+        }
+    }
+
+    public class FooCommand<T> : CommandLineApplication<T> where T:class {
+        T foo { get; set; }
+        public FooCommand() : base(){
+            }
+
+
+        public void Foo() {
+            var f = new CommandLineApplication<String>();
+        }
+    }
+
+}
--- src/TemplatesConsole/Program.cs
+++ src/TemplatesConsole/Program.cs
@@ -3,24 +3,76 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using TemplatesShared;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Diagnostics;
+using McMaster.Extensions.CommandLineUtils;
 
-namespace TemplatesConsole
-{
-    class Program
-    {
-        static async System.Threading.Tasks.Task Main(string[] args)
-        {
+namespace TemplatesConsole {
+    class Program {
+        static void Main(string[] args) {
+            new Program(args).Run();
+        }
+
+        private string[] _args;
+        private ServiceCollection _services = null;
+        private ServiceProvider _serviceProvider = null;
+
+        private Program(string[] args) {
+            _args = args;
+            RegisterServices();
+        }
+
+        private void Run() {
+            //using var app = new CommandLineApplication<Program> {
+            //    Name = "templatereport",
+            //    UsePagerForHelpText = false
+            //};
+
+            using var app = new CommandLineApplication<Program>();
+
+            app.ExtendedHelpText = @"
+            Remarks:
+              Typical hello app.
+            ";
+
+            app.Conventions
+                .UseDefaultConventions()
+                .UseConstructorInjection();
+
+            app.HelpOption(true);
+
+            // add sub commands here
+            app.AddSubcommand(new HelloCommand<Program>());
+
+            app.Execute(_args);
+        }
+
+        private void OnExecute() {
+            Console.WriteLine("OnExecute");
+        }
+
+        private void RegisterServices() {
+            _services = new ServiceCollection();
+
+        }
+
+        private TType GetFromServices<TType>() {
+            Debug.Assert(_serviceProvider != null);
+            return _serviceProvider.GetRequiredService<TType>();
+        }
+
+        static async System.Threading.Tasks.Task MainOld(string[] args) {
             string baseurl = "https://azuresearch-usnc.nuget.org/query";
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(baseurl);
             var result = await client.GetStringAsync(@"?q=sidewaffle&take=50&prerelease=true");
 
-            try
-            {
+            try {
                 var list = JsonConvert.DeserializeObject<NuGetSearchApiResult>(result);
             }
-            catch(Exception ex)
-            {
+            catch (Exception ex) {
                 System.Console.WriteLine(ex.ToString());
             }
             // var result = JsonConvert.DeserializeObject<List<TemplatePack>>(text);
