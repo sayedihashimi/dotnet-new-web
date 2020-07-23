1bd9b267f31ee711729b9bd3861c59ac267cd841
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-07-22 23:13:41 -0400

    adding remotefile class

---
+++ src/TemplateTest/RemoteFileTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TemplatesShared;
+using Xunit;
+
+namespace TemplateTest {
+    public class RemoteFileTests {
+
+        [Fact]
+        public void TestGetRemoteFileThatIsNotPresentLocally() {
+            // todo: would be better to externalize the download part so that can be mocked for test
+            string urlToDownload = @"https://raw.githubusercontent.com/sayedihashimi/dotnet-new-web/master/src/template-report.json";
+            string filename = Guid.NewGuid().ToString();
+            var remoteFile = new RemoteFile();
+            var localFile = remoteFile.GetRemoteFile(urlToDownload, filename);
+
+            Assert.True(File.Exists(localFile), $"local file not found at '{localFile}'");
+        }
+    }
+}
---
+++ src/TemplatesShared/RemoteFile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace TemplatesShared {
+    public interface IRemoteFile {
+        bool Verbose { get; set; }
+
+        string GetRemoteFile(string downloadUrl, string filename, bool forceDownload = false);
+        string GetRemoteFile(Uri uri, string filename, bool forceDownlaod = false);
+        void SetCacheFolderpath(string folderpath);
+    }
+
+    public class RemoteFile : IRemoteFile {
+        protected string CacheFolderpath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "templatereport");
+        // todo: should have a better way to conditionally print to the console
+        public bool Verbose { get; set; }
+        public void SetCacheFolderpath(string folderpath) {
+            if (string.IsNullOrEmpty(folderpath)) {
+                throw new ArgumentNullException(nameof(folderpath), $"{nameof(folderpath)} is empty");
+            }
+
+            CacheFolderpath = folderpath;
+        }
+
+        public string GetRemoteFile(string downloadUrl, string filename, bool forceDownload = false) {
+            if (string.IsNullOrEmpty(downloadUrl)) {
+                throw new ArgumentNullException(nameof(downloadUrl), $"{nameof(downloadUrl)} is empty");
+            }
+            return GetRemoteFile(new Uri(downloadUrl), filename, forceDownload);
+        }
+        public string GetRemoteFile(Uri uri, string filename, bool forceDownlaod = false) {
+            // check to see if the file has already been downloaded
+            var expectedFilePath = GetLocalFilepathFor(filename);
+            if (File.Exists(expectedFilePath) && forceDownlaod) {
+                File.Delete(expectedFilePath);
+            }
+
+            // if the file exists at the expected location return it
+            if (File.Exists(expectedFilePath)) {
+                return expectedFilePath;
+            }
+
+            if (!Directory.Exists(Path.GetDirectoryName(expectedFilePath))) {
+                Directory.CreateDirectory(Path.GetDirectoryName(expectedFilePath));
+            }
+
+            // download the file now
+            var webClient = new System.Net.WebClient();
+            webClient.DownloadFile(uri, expectedFilePath);
+
+            return expectedFilePath;
+        }
+
+        protected string GetLocalFilepathFor(string filename) {
+            return Path.Combine(CacheFolderpath, filename);
+        }
+    }
+}
