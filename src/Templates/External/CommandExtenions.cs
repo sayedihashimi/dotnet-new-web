8c904e7cc40b85190f57672959e0d2615b360c94
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-08-20 23:19:12 -0400

    adding new project with hello world content

---
+++ src/Templates/External/CommandExtenions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Text;
+
+namespace Templates {
+    // from: https://github.com/dotnet/diagnostics/blob/master/src/Tools/Common/CommandExtensions.cs
+    public static class CommandExtenions {
+        /// <summary>
+        /// Allows the command handler to be included in the collection initializer.
+        /// </summary>
+        public static void Add(this Command command, ICommandHandler handler) {
+            command.Handler = handler;
+        }
+    }
+}
---
+++ src/Templates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Templates {
+    class Program {
+        public static Task<int> Main(string[] args) {
+            var parser = new CommandLineBuilder()
+                            .AddCommand(SearchCommand())
+                            .UseDefaults()
+                            .Build();
+
+            return parser.InvokeAsync(args);
+        }
+
+        private static Command SearchCommand() =>
+            new Command(name: "hello", description: "hello command") {
+                CommandHandler.Create<string>( (searchTerm) => {
+                    Console.WriteLine($"hello {searchTerm}"); }
+                ),
+                ArgSearchTerm()
+            };
+
+        private static Argument ArgSearchTerm() =>
+            new Argument<string>(
+                name: "search-term") {
+                Description = "search term"
+            };
+    }
+}
