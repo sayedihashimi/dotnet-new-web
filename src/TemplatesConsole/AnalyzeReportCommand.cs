e19ed4a8c59439e00ff733f70af9cbce8c52bc35
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-09-01 22:44:31 -0400

    refactoring analyze command

--- src/TemplatesConsole/AnalyzeReportCommand.cs
+++ src/TemplatesConsole/AnalyzeReportCommand.cs
@@ -51,62 +51,90 @@ namespace TemplatesConsole {
                 // 1. packages which are using PackageType != template
                 // 2. packages with dependency assembly files (check for a lib folder)
 
-                // write the results to a temp file, and then copy to the final destination
-                var tempfilepath = Path.GetTempFileName();
                 var templatePacks = TemplatePack.CreateFromFile(templateReportJsonPath);
-                using var writer = new StreamWriter(tempfilepath);
-                writer.WriteLine("package name, version, has lib folder, packagetype");
-                foreach (var tp in templatePacks) {
-                    var info = new TemplatePackReportInternalSummaryInfo(_remoteFile.CacheFolderpath, tp);
-                    var line = GetTemplatePackReportLineFor(info);
-                    Console.WriteLine(line);
-                    writer.WriteLine(line);
-                }
-                writer.Flush();
-                writer.Close();
-
+                
+                // write the results to a temp file, and then copy to the final destination
                 string resultsPath = optionAnalysisResultFilePath.HasValue() ? optionAnalysisResultFilePath.Value() : "template-pack-analysis.csv";
-                Console.WriteLine($"writing analysis file to {resultsPath}");
-                File.Copy(tempfilepath, resultsPath, true);
+                CreateTemplatePackFile(templatePacks, templateReportJsonPath, resultsPath);
 
-                // now create the template-details.csv file
+                // create the json file that contains all the templates                
                 var allTemplates = new List<Template>();
                 var allTemplateInfos = new List<TemplateReportSummaryInfo>();
                 foreach (var tp in templatePacks) {
                     var extractFolderPath = Path.Combine(_remoteFile.CacheFolderpath, "extracted", ($"{tp.Package}.{tp.Version}.nupkg").ToLowerInvariant());
                     var templates = TemplatePack.GetTemplateFilesUnder(extractFolderPath);
-                    foreach(var template in templates) {
+                    foreach (var template in templates) {
                         var templateObj = Template.CreateFromFile(template);
                         allTemplates.Add(templateObj);
                         allTemplateInfos.Add(new TemplateReportSummaryInfo { Template = templateObj });
                     }
                 }
 
-                // write out the json file that contains all the templates
-                var tempTemplateDetailsFilepath = Path.GetTempFileName();
-                var allTemplatesJson = JsonConvert.SerializeObject(allTemplates,Formatting.Indented);
-                File.WriteAllText(tempTemplateDetailsFilepath, allTemplatesJson);
-                // file path is same as the results but name has -templates and extension is .json
-                var templatesJsonFilePath = $"{resultsPath.Substring(0, resultsPath.Length - 5)}-templates.json";
-                File.Copy(tempTemplateDetailsFilepath, templatesJsonFilePath, true);
+                CreateAllTemplatesJsonFile(allTemplates, resultsPath);
 
                 // create the template-details.csv file now
-                var templateDetailsTempFilePath = Path.GetTempFileName();
-                using var templateDetailsWriter = new StreamWriter(templateDetailsTempFilePath);
-                templateDetailsWriter.WriteLine("name,template-type,author,sourceName,defaultName,baseline,language,primaryOutputs,tags,identity,groupIdentity");
-                foreach (var templateInfo in allTemplateInfos) {
-                    templateDetailsWriter.WriteLine(GetTemplateDetailsReportLineFor(templateInfo));
-                }
-                templateDetailsWriter.Flush();
-                templateDetailsWriter.Close();
+                CreateTemplateDetailsCsvFile(allTemplateInfos, resultsPath);
 
-                var templateDetailsDestPath = Path.Combine(Path.GetDirectoryName(resultsPath),"template-details.csv");
-                File.Copy(templateDetailsTempFilePath, templateDetailsDestPath, true);
-                
-                //$"{resultsPath.Substring(0, resultsPath.Length - 5)}-templates.json";
                 return 1;
             };
         }
+
+        private void CreateTemplatePackFile(List<TemplatePack> templatePacks,string templateReportJsonPath, string resultsPath) {
+            var tempfilepath = Path.GetTempFileName();
+            //var templatePacks = TemplatePack.CreateFromFile(templateReportJsonPath);
+            using var writer = new StreamWriter(tempfilepath);
+            writer.WriteLine("package name, version, has lib folder, packagetype");
+            foreach (var tp in templatePacks) {
+                var info = new TemplatePackReportInternalSummaryInfo(_remoteFile.CacheFolderpath, tp);
+                var line = GetTemplatePackReportLineFor(info);
+                Console.WriteLine(line);
+                writer.WriteLine(line);
+            }
+            writer.Flush();
+            writer.Close();
+
+            // string resultsPath = optionAnalysisResultFilePath.HasValue() ? optionAnalysisResultFilePath.Value() : "template-pack-analysis.csv";
+            Console.WriteLine($"writing analysis file to {resultsPath}");
+            File.Copy(tempfilepath, resultsPath, true);
+
+            // now create the template-details.csv file
+            var allTemplates = new List<Template>();
+            var allTemplateInfos = new List<TemplateReportSummaryInfo>();
+            foreach (var tp in templatePacks) {
+                var extractFolderPath = Path.Combine(_remoteFile.CacheFolderpath, "extracted", ($"{tp.Package}.{tp.Version}.nupkg").ToLowerInvariant());
+                var templates = TemplatePack.GetTemplateFilesUnder(extractFolderPath);
+                foreach (var template in templates) {
+                    var templateObj = Template.CreateFromFile(template);
+                    allTemplates.Add(templateObj);
+                    allTemplateInfos.Add(new TemplateReportSummaryInfo { Template = templateObj });
+                }
+            }
+        }
+
+        private void CreateAllTemplatesJsonFile(List<Template> allTemplates, string resultsPath) {
+            // write out the json file that contains all the templates
+            var tempTemplateDetailsFilepath = Path.GetTempFileName();
+            var allTemplatesJson = JsonConvert.SerializeObject(allTemplates, Formatting.Indented);
+            File.WriteAllText(tempTemplateDetailsFilepath, allTemplatesJson);
+            // file path is same as the results but name has -templates and extension is .json
+            var templatesJsonFilePath = $"{resultsPath.Substring(0, resultsPath.Length - 4)}-templates.json";
+            File.Copy(tempTemplateDetailsFilepath, templatesJsonFilePath, true);
+        }
+
+        private void CreateTemplateDetailsCsvFile(List<TemplateReportSummaryInfo> allTemplateInfos, string resultsPath) {
+            var templateDetailsTempFilePath = Path.GetTempFileName();
+            using var templateDetailsWriter = new StreamWriter(templateDetailsTempFilePath);
+            templateDetailsWriter.WriteLine("name,template-type,author,sourceName,defaultName,baseline,language,primaryOutputs,tags,identity,groupIdentity");
+            foreach (var templateInfo in allTemplateInfos) {
+                templateDetailsWriter.WriteLine(GetTemplateDetailsReportLineFor(templateInfo));
+            }
+            templateDetailsWriter.Flush();
+            templateDetailsWriter.Close();
+
+            var templateDetailsDestPath = Path.Combine(Path.GetDirectoryName(resultsPath), "template-details.csv");
+            File.Copy(templateDetailsTempFilePath, templateDetailsDestPath, true);
+        }
+
         private string ReplaceComma(string str) {
             if (string.IsNullOrEmpty(str)) {
                 return string.Empty;
