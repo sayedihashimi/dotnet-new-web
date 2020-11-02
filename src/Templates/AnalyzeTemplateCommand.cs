feaf983d5ad0c1a0c74209acc06befc499e4c6bf
Tim Heuer <tim@timheuer.com>
2020-11-02 15:20:25 -0800

    Updating path search
    This accounts for "." directories on both Windows and Linux (currently fails if you say `analyze -f .` on Linux)

--- src/Templates/AnalyzeTemplateCommand.cs
+++ src/Templates/AnalyzeTemplateCommand.cs
@@ -50,7 +50,7 @@ namespace Templates {
 
                         foreach(var f in folders) {
                             // finding folders under f that has a .template.config folder
-                            var foundDirs = Directory.GetDirectories(f,".template.config",new EnumerationOptions{RecurseSubdirectories = true });
+                            var foundDirs = Directory.GetDirectories(f,".template.config",new EnumerationOptions{RecurseSubdirectories = true, ReturnSpecialDirectories = true });
                             if(foundDirs == null || foundDirs.Length <= 0) {
                                 _reporter.WriteLine($"ERROR: No templates found under path '{f}'");
                             }
