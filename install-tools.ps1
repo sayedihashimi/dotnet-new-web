4c296593159df772c444a2c5b63b808108004acc
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2021-04-25 16:53:02 -0400

    ci update - wip

--- install-tools.ps1
+++ install-tools.ps1
@@ -7,22 +7,29 @@ function DeleteCacheFolders{
         [string[]]$commandName = ("templates","templatereport")
     )
     process{
+        'DeleteCacheFolders' | Write-Output
         foreach($cn in $commandName){
+            'DeleteCacheFolders1, cn="{0}"' -f $cn | Write-Output
             $exepath = Join-Path $env:HOME .dotnet\tools\ ("{0}.exe" -f $cn)
+            ' exepath: "{0}"' -f $exepath | Write-Output
             if(Test-Path $exepath -PathType Leaf){
                 Remove-Item -LiteralPath $exepath -Force
             }                        
         }
         foreach($tn in $toolNames){
+            'DeleteCacheFolders1, cn="{0}"' -f $cn | Write-Output
             $cacheFolder = Join-Path $env:HOME .dotnet\tools\.store $tn
+            ' cacheFolder: "{0}"' -f $cacheFolder | Write-Output
             if(Test-Path $cacheFolder -PathType Container){
                 Remove-Item -LiteralPath $cacheFolder -Recurse -Force
             }
 
             # delete nuget package cache files as well
             $nugetcachefolder = Join-Path $env:LOCALAPPDATA NuGet\v3-cache
+            ' nugetcachefolder: "{0}"' -f $nugetcachefolder | Write-Output
             [string[]]$foundnugetfiles = Get-ChildItem -Path $nugetcachefolder ("*{0}*" -f $tn) -Recurse -File
             if($foundnugetfiles -and $foundnugetfiles.Length -gt 0){
+                ' foundnugetfiles.Length: "{0}"' -f ($foundnugetfiles.Length) | Write-Output
                 Remove-Item -LiteralPath $foundnugetfiles -Force
             }
             #Get-ChildItem -Path $nugetcachefolder ("*{0}*" -f $cn) -Recurse -ErrorAction Ignore | Remove-Item -Force -ErrorAction SilentlyContinue
