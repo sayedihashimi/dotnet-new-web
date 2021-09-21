4c9a9eb8ec767f965950b2861311817a73b3d786
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2021-09-21 17:30:03 -0400

    minor changes

---
+++ src/TemplatesShared/VsixAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplatesShared {
+    public class VsixAnalyzer {
+        /// <summary>
+        /// Things to check for:
+        /// 1. Ensure there is a pkgdef file with something like the following
+        ///     [$RootKey$\TemplateEngine\Templates\sayedha.template.netcoretool.nuspec\1.0.0]
+        ///     "InstalledPath"="$PackageFolder$\ProjectTemplates"
+        ///    To get the list of .pkgdef files to check look for <assets> tag in the .vsixmanifest
+        /// 2. Ensure that there is a .nupkg file that contains 1 or more templates
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="pathToVsix"></param>
+        /// <returns></returns>
+        bool Analyze(string pathToVsix) {
+            throw new NotImplementedException();
+        }
+    }
+}
